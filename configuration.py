312c027eec6b1a3f15a7674eb0680e690b1f025b
OlivierNDO <Olivier_N@lynchburg.edu>
2020-07-30 12:11:45 -0500

    initial configuration commit

---
+++ configuration.py
@@ -0,0 +1,17 @@
+# File Path to Data Sourced from https://www.consumerfinance.gov/data-research/consumer-complaints/#download-the-data
+config_complaints_file = 'D:/complaint_data/complaints.csv'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
